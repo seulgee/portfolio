8ae525035fdf3104f0c062db712a9a1ffebe3d9a
seulgee <35548897+seulgee@users.noreply.github.com>
2018-11-28 02:12:52 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function (){
+
+function resizeHeaderOnScroll() {
+  const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+  shrinkOn = 200,
+  headerEl = document.getElementById('js-header');
+  
+  if (distanceY > shrinkOn) {
+    headerEl.classList.add("smaller");
+  } else {
+    headerEl.classList.remove("smaller");
+  }
+}
+
+window.addEventListener('scroll', resizeHeaderOnScroll);
+
+});
