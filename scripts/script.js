3e2d9b9c4f9d612d62de925f4b28f493ef921578
Seulgee Lee <seulgee.lee97@gmail.com>
2019-09-24 00:28:32 -0500

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 $(window).scroll(function() {
 
-  var upArrow = $('#up-arrow')
+   var upArrow = $('#up-arrow')
+   var filter = $('.button-group-mobile')
+   var toTop = $('.to-top')
+
   if ($(this).scrollTop()>600)
    {
       upArrow.fadeIn(300);
@@ -10,4 +13,16 @@ $(window).scroll(function() {
    {
     upArrow.fadeOut(300);
    }
+
+   if (toTop.is(":visible")) {
+      if ($(this).scrollTop()>30)
+         {
+            filter.fadeOut(200);
+            console.log('working')
+         }
+      else
+         {
+         filter.fadeIn(200);
+         }
+   } else {}
 });
